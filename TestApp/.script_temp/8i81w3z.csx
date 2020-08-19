392a39936414186e775288576f67e3bec67484bc
tylermcleant <66703959+tylermcleant@users.noreply.github.com>
2020-08-19 19:25:57 -0400

    First lines of C#

---
+++ TestApp/.script_temp/8i81w3z.csx
@@ -0,0 +1,3 @@
+Console.WriteLine("Hello World!");
+Console.Write("Tyler! ");
+object p = Console.Write("You wrote your first lines of code!");
---
+++ TestApp/helloworld.cs
@@ -0,0 +1,5 @@
+//console.WriteLine('Hello World ' + 2);
+
+Console.WriteLine("Hello World!");
+Console.Write("Tyler! ");
+object p = Console.Write("You wrote your first lines of code!");
